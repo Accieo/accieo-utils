dec7523e68317450721fc17fb37e1063cbeb7d13
Accieo <42718776+Accieo@users.noreply.github.com>
2023-08-26 14:30:17 +0200

    Initial Commit

---
+++ AccieoUtils/AccieoUtilsApp.swift
@@ -0,0 +1,17 @@
+//
+//  AccieoUtilsApp.swift
+//  AccieoUtils
+//
+//  Created by Accieo on 26.08.23.
+//
+
+import SwiftUI
+
+@main
+struct AccieoUtilsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
